60dbb0ee8a2f3c9c05d345fa4453cf6430bcb37d
Alex Solov <Alextxnk@users.noreply.github.com>
2020-08-21 02:44:32 +0300

    style

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 let guessTheNumber = prompt('Угадай число от 1 до 100');
 
-  function guessNumber(){
-
-    function getRandomInt(min, max) {
+function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; 
     };
   
   const number = getRandomInt(1, 100);
   console.dir(number);
+
+  function guessNumber(){
 
       if(guessTheNumber > number){
           guessTheNumber = prompt('Загаданное число меньше');
@@ -17,7 +17,7 @@ let guessTheNumber = prompt('Угадай число от 1 до 100');
       } else if (guessTheNumber < number){
         guessTheNumber = prompt('Загаданное число больше');
           return guessNumber();
-      } else if (!guessTheNumber(num)){
+      } else if (!Number(guessTheNumber)){
         guessTheNumber = prompt('Введите число!');
           return guessNumber();
       } else if (typeof(guessTheNumber) === false){
