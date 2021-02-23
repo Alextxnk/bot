ef02bc2ba2af9979ab43b7453bd79e143274683a
Alex Solov <Alextxnk@users.noreply.github.com>
2021-02-24 00:08:12 +0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 'use strict';
 
-let 
+const arr_en = new Set (['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
+const arr_EN = new Set (['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']);
+
+//for (let value of set) alert(value);
+
+let lang;
+let A = new Set();
+
+lang = prompt('Введите элементы множества');
+console.log('Число элементов во множестве: ',lang.length);
+
+console.log()
