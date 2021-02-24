c61fc3449f6db4d0ded3c7a69bca4a2ae11b0d10
Alex Solov <Alextxnk@users.noreply.github.com>
2021-02-24 11:34:06 +0300

    style

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 'use strict';
 
-const arr_en = new Set (['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
-const arr_EN = new Set (['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']);
+let A = new Set();
+let item;
+const arrEn = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //for (let value of set) alert(value);
 
-let lang;
-let A = new Set();
+let lang = new Set(arrEn);
 
-lang = prompt('Введите элементы множества');
-console.log('Число элементов во множестве: ',lang.length);
+/*
+langEn = prompt('Введите элементы множества');
 
-console.log()
+
+console.log();
+*/
+item = prompt("Введите элементы множества");    
+    
+if (item === lang) {
+    A.add(item);
+}
+console.log('Число элементов во множестве: ',item.length);
+
+/*
+const printset = function (){
+    return('');
+};
+console.log(printset);
+*/
